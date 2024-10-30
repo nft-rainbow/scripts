ba4f03655536bce0893a4a25910c49d80219d292
Pana <pana.wang@outlook.com>
2024-10-30 13:58:39 +0800

    up sync logic

--- sponsorLogSyncer.js
+++ sponsorLogSyncer.js
@@ -13,7 +13,7 @@ main();
 async function main() {
     setInterval(async function() {
         try {
-            await fetchAndSaveSponsorLog(SPONSOR_ADDRESS);
+            fetchAndSaveSponsorLog(SPONSOR_ADDRESS);
         }catch(e) {
             console.log(new Date(), 'fetchAndSaveSponsorLog error', e);
         }
@@ -21,81 +21,101 @@ async function main() {
 }
 
 async function fetchAndSaveSponsorLog(account) {
-    const txs = await getUserTx(account);
+    let lastItems = await SponsorLog.findAll({
+        offset: 1, // skip the first item, which is a refunded tx
+        limit: 1,
+        order: [
+            ['nonce', 'DESC']
+        ]
+    });
+    let dbLastNonce = lastItems[0] ? lastItems[0].nonce : 0;
+    console.log(new Date(), 'DB last nonce:', dbLastNonce);
+
+    let blockChainLastNonce = await conflux.cfx.getNextNonce(account);
+    console.log(new Date(), 'BlockChain last nonce:', blockChainLastNonce);
+    if (dbLastNonce + 1 == blockChainLastNonce) return; // no need to update
+
+    const txs = await getAllUserTx(account, skip = dbLastNonce, 100, 'ASC');
 
     for(let tx of txs) {
-        let value = new Drip(tx.value);
-        let cfxCount = value.toCFX();
-        let methodArg = sponsorContract.abi.decodeData(tx.txInfo.data);
-        const contract = methodArg.object.contractAddr;
-        let meta = {
-            sponsor_at: new Date(tx.timestamp * 1000),
-            type: tx.method.startsWith('setSponsorForCollateral') ? 2 : 1, // 1-gas 2-storage
-            value: parseInt(cfxCount),
-            nonce: parseInt(tx.nonce),
-            hash: tx.hash,
-            from: tx.from,
-            to: tx.to,
-            contract,
-            created_at: new Date(),
-        };
-
-        let exist = await SponsorLog.findOne({ where: { nonce: meta.nonce } });
-        if (exist) {
-            continue;
-        }
-
-        try {
-            console.log(new Date(), 'Save sponsor log', meta.nonce);
-            await SponsorLog.create(meta);
-        } catch(e) {
-            if (e.message !== 'Validation error') {
-                console.log('Log create error', e);
-            }
-        }
-
-        await wait(2000);
+        await createSponsorLogForTx(tx);
     }
 }
 
-async function getUserTx(account, limit = 100) {
-    let skip = 0;
+async function createSponsorLogForTx(tx) {
+    let nonce = parseInt(tx.nonce);
+
+    let exist = await SponsorLog.findOne({ where: { nonce: meta.nonce } });
+    if (exist) return false;
+
+    let value = new Drip(tx.value);
+    let cfxCount = value.toCFX();
+    let methodArg = sponsorContract.abi.decodeData(tx.txInfo.data);
+    const contract = methodArg.object.contractAddr;
+    let meta = {
+        sponsor_at: new Date(tx.timestamp * 1000),
+        type: tx.method.startsWith('setSponsorForCollateral') ? 2 : 1, // 1-gas 2-storage
+        value: parseInt(cfxCount),
+        nonce,
+        hash: tx.hash,
+        from: tx.from,
+        to: tx.to,
+        contract,
+        created_at: new Date(),
+    };
+
+    try {
+        console.log(new Date(), 'Save sponsor log', meta.nonce);
+        await SponsorLog.create(meta);
+    } catch(e) {
+        if (e.message !== 'Validation error') {
+            console.log('Log create error', e);
+        } else {
+            console.log('Log already exist', e);
+        }
+        return false;
+    }
+
+    return true;
+}
+
+// get user first 100 txs, order by nonce desc
+async function getUserTx(account, skip = 0, limit = 100) {
     let txs = [];
     // Get 'account' tx from Scan API
     let {list, total} = await _getUserTx(account, skip, limit);
     txs = txs.concat(list);
     // Get tx detail Info from RPC
     for(let i in txs) {
-        let tx = txs[i];
-        let txInfo = await conflux.cfx.getTransactionByHash(tx.hash);
+        let txInfo = await conflux.cfx.getTransactionByHash(txs[i].hash);
         txs[i].txInfo = txInfo;
+        await wait(500); // 避免 rpc 请求频率过快
     }
     return txs;
 }
 
-async function getAllUserTx(account) {
-    let skip = 0;
-    let limit = 50;
+// get all user txs, skip the first 'skip' txs
+async function getAllUserTx(account, skip = 0, limit = 100, sort = 'DESC') {
     let txs = [];
     // Get 'account' tx from Scan API
     while (true) {
-        let {list, total} = await _getUserTx(account, skip, limit);
+        let {list, total} = await _getUserTx(account, skip, limit, sort);
         txs = txs.concat(list);
-        if (txs.length >= total) break;
+        if (list.length < limit) break;
         skip += limit;
     }
     // Get tx detail Info from RPC
     for(let i in txs) {
-        let tx = txs[i];
-        let txInfo = await conflux.cfx.getTransactionByHash(tx.hash);
+        let txInfo = await conflux.cfx.getTransactionByHash(txs[i].hash);
         txs[i].txInfo = txInfo;
+        await wait(500); // 避免 rpc 请求频率过快
     }
     return txs;
 }
 
-async function _getUserTx(account, skip, limit) {
+async function _getUserTx(account, skip, limit, sort = 'DESC') {
     const HOST = 'https://api.confluxscan.net';
-    const { data } = await axios.get(`${HOST}/account/transactions?account=${account}&from=${account}&skip=${skip}&limit=${limit}`);
+    const { data } = await axios.get(`${HOST}/account/transactions?account=${account}&from=${account}&skip=${skip}&limit=${limit}&sort=${sort}`);
     return data.data;
 }
 
